03cae9f19ab7061a28d01419dc1495e16952f715
BytesCrafter <bytescrafter@gmail.com>
2023-04-28 20:22:12 +0800

    Added motion detections via accellation.

--- src/app/services/mobile.service.ts
+++ src/app/services/mobile.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { StatusBar, Style } from '@capacitor/status-bar';
 import { Preferences } from '@capacitor/preferences';
+import { PluginListenerHandle } from '@capacitor/core';
+import { Motion } from '@capacitor/motion';
+let accelHandler: PluginListenerHandle;
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +14,36 @@ export class MobileService {
     window.addEventListener('statusTap', function () { // iOS only
       console.log('statusbar tapped');
     });
+  }
+
+  async startMotionListener() {
+    // try {
+    //   await DeviceMotionEvent.requestPermission();
+    // } catch (e) {
+    //   // Handle error
+    //   return;
+    // }
+
+    // Once the user approves, can start listening:
+    accelHandler = await Motion.addListener('accel', event => {
+      console.log('Device motion event:', event);
+    });
+  }
+
+  stopMotionListener() {
+    // Stop the acceleration listener
+    const stopAcceleration = () => {
+      if (accelHandler) {
+        accelHandler.remove();
+      }
+    };
+  }
+
+  removeMotionListener() {
+    // Remove all listeners
+    const removeListeners = () => {
+      Motion.removeAllListeners();
+    };
   }
 
   async setPrefs(keyName: any = '', strvar: any = '') {
